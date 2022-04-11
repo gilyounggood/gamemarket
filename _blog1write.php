66952a6f3775223db40914f7b8597639a9344060
gilyounggood <https://github.com/gilyounggood/gamemarket.git>
2022-04-11 19:04:46 +0900

    커뮤니티 이미지 파일 업로드 추가

--- _blog1write.php
+++ _blog1write.php
@@ -6,8 +6,20 @@
     $category = $_POST['category'];
     $content = $_POST['content'];
 
+    $img_file = $_FILES['img_file'];
+    $img_size = $_FILES['img_file'];['size'];
+
+    require('_file_upload.php');
+
+    if($result) {
+        // 파일이 있을 경우
+        $sql = "INSERT INTO `blog1` (`name`, `title`, `category`, `content`, `id`, `img_file`, `img_size`) VALUE ('$name', '$title', '$category', '$content', '$id', '$file_name', '$img_size')";   
+    } else {
+        // 파일이 없는 경우
+        $sql = "INSERT INTO `blog1` (`name`, `title`, `category`, `content`, `id`) VALUE ('$name', '$title', '$category', '$content', '$id')";
+    }
+
     require('_conn.php');
-    $sql = "INSERT INTO `blog1` (`name`, `title`, `category`, `content`, `id`) VALUE ('$name', '$title', '$category', '$content', '$id')";
     $result = mysqli_query($conn, $sql);
     if ($result) {
         echo "<script>alert('글 작성이 완료되었습니다.');location.href='blog.php'</script>";        
